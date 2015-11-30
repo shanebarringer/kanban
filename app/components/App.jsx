9aca23ac4da4d387a0f203d6102d450412145317
Shane Barringer <shane.barringer@outlook.com>
2015-11-29 21:28:12 -0500

    adds 'model data' for notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,36 @@
+import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note.jsx';
 
-export default function(){
-  return <Note />;
-};
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn Webpack'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+];
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+
+  renderNote(note){
+    return (
+      <li key={note.id}>
+        <Note task={note.task} />
+      </li>
+    );
+  }
+}
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Note extends React.Component {
   render(){
-    return <div> Learning Webpack! </div>
+    return <div> {this.props.task} </div>
   }
 }
